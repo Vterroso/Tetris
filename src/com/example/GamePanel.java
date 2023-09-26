114d604ae289cbad3a9e52b9006cdf18bf6aa35f
Victor Terroso <v.terroso87@gmail.com>
2023-09-26 19:08:29 +0200

    Initial commit

---
+++ src/com/example/GamePanel.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable{
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
+    PlayManager pm;
+
+    public GamePanel(){
+
+        //Panel settings
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.DARK_GRAY);
+        this.setLayout(null);
+        // Implement Keylistener
+        this.addKeyListener(new KeyHandler());
+        this.setFocusable(true);
+
+        pm = new PlayManager();
+    }
+    public void launchGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        //Game Loop
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lasTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lasTime) / drawInterval;
+            lasTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta --;
+            }
+        }
+    }
+        private void update(){
+            pm.update();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        pm.draw(g2);
+    }
+
+}
